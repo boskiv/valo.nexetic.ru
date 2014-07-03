9880ddae16b58ee3afecd42077118b42c7698059
Ivan Skiridomov <Ivan.Skiridomov@gmail.com>
2014-07-03 16:35:32 +0400

    Add survey table

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by skivan on 03/07/14.
+ */
+$(function() {
+    $("#create-button").click(function(){
+        console.log("Hide and show")
+        $("#reg-form").hide();
+        $("#survey-form").show(500);
+    })
+
+    $("[name='my-checkbox']").bootstrapSwitch();
+});
