f1a00dbbb001781245a8bff9a2877ce558e037a1
Ivan Skiridomov <Ivan.Skiridomov@nexetic.com>
2014-07-03 18:02:51 +0400

    Send button event

--- js/app.js
+++ js/app.js
@@ -3,10 +3,17 @@
  */
 $(function() {
     $("#create-button").click(function(){
-        console.log("Hide and show");
+
         $("#reg-form").hide();
         $("#survey-form").show(500);
     });
 
     $("[name='my-checkbox']").bootstrapSwitch();
+
+    $("#send-button").click(function(){
+
+        $("#survey-form").hide();
+        $("#reg-form").show(500);
+
+    });
 });
