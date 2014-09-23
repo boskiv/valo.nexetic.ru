6712add76bb6cbbf7b884e21f484d43bc3e065f8
Ivan Skiridomov <ivan.skiridomov@nexetic.com>
2014-09-24 02:24:37 +0400

    send Form Data

--- js/app.js
+++ js/app.js
@@ -9,7 +9,17 @@ $(function() {
     });
 
     $("#send-button").click(function(){
-
+        //var formData = JSON.stringify($("#leed-form").serializeArray());
+        var fd = new FormData(document.getElementById("leed-form"));
+        fd.append("CustomField", "This is some extra data");
+        $.ajax({
+            url: "http://valo.nexetic.ru/upload.php",
+            type: "POST",
+            data: fd,
+            processData: false,  // tell jQuery not to process the data
+            contentType: false   // tell jQuery not to set contentType
+        });
+        console.log(fd);
         $("#survey-form").hide();
         $("#reg-form").show(500);
 
